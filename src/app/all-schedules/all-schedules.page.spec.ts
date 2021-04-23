0bbb857e440fdf7a8da8b0b6778ec8524498d0a3
Cameron Kleiman <camtheman256@gmail.com>
2021-04-23 20:30:47 +0000

    update angular version

--- src/app/all-schedules/all-schedules.page.spec.ts
+++ src/app/all-schedules/all-schedules.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AllSchedulesPage } from './all-schedules.page';
@@ -7,7 +7,7 @@ describe('AllSchedulesPage', () => {
   let component: AllSchedulesPage;
   let fixture: ComponentFixture<AllSchedulesPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AllSchedulesPage ],
       imports: [IonicModule.forRoot()]
--- src/app/current-schedule/current-schedule.page.spec.ts
+++ src/app/current-schedule/current-schedule.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { CurrentSchedulePage } from './current-schedule.page';
@@ -7,7 +7,7 @@ describe('CurrentSchedulePage', () => {
   let component: CurrentSchedulePage;
   let fixture: ComponentFixture<CurrentSchedulePage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CurrentSchedulePage ],
       imports: [IonicModule.forRoot()]
